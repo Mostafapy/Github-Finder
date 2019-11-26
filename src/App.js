0f8cd77fb110cbcf967832882dcaa6a37d6045c7
MostafaHisham <mostafa.hisham@digi2.com>
2019-11-26 16:31:55 +0200

    app.js refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Component}from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+       <h1>Hello</h1>
       </div>
     );
   }
