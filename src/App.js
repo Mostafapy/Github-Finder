d0d65953bf0aeefcf46b4de58f55363db652e05c
MostafaHisham <mostafa.hisham@digi2.com>
2019-12-04 17:00:29 +0200

    add searchUsers props function in  app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -9,7 +10,8 @@ class App extends Component {
     users: [],
     loading: false,
   };
-  async componentDidMount() {
+
+  /*async componentDidMount() {
     this.setState({ loading: true });
 
     const res = await axios.get(
@@ -17,12 +19,24 @@ class App extends Component {
     );
 
     this.setState({ users: res.data, loading: false });
+  }*/
+
+  // Search GitHub Users
+  searchUsers = async txt => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${txt}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    );
+
+    this.setState({ users: res.data.items, loading: false });
   }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
