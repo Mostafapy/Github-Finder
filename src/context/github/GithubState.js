2c12be169cb30541b79101052f24e98df825a2a9
MostafaHisham <mostafa.hisham@digi2.com>
2019-12-12 15:58:55 +0200

    intialize GithubState.jks

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,12 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  GET_REPOS,
+  GET_USERS,
+  CLEAR_USERS,
+} from '../types';
